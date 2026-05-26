25ba56d071b23c4e8c6f8ce0bfcfec8eed2e2323
Katchuga <biel2005pereira@gmail.com>
2026-05-26 18:21:47 -0300

    conexão com o banco de dados, criação das DAOS e teste com o banco de dados

---
+++ src/main/java/br/com/fatec/DAO/ProdutoDAO.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.fatec.DAO;
+
+import br.com.fatec.model.Produto;
+import br.com.fatec.persistencia.Banco;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+
+/**
+ *
+ * @author biel2
+ */
+public class ProdutoDAO implements DAO <Produto>{
+//variaveis auxiliares
+    private Produto produto;
+    //auxiliares para acesso aos dados
+    
+    //para conter os comandos DML
+    private PreparedStatement pst; //pacote java.sql
+    //para conter os dados vindos do BD
+    private ResultSet rs; //pacote java.sql
+    
+    @Override
+    public boolean insere(Produto model) throws SQLException {
+        
+        String sql = "INSERT INTO produto (id, nome, prescricao, descricao, preco) "
+        + "VALUES (?, ?, ?, ?, ?);";
+        
+        //Abre a conexao
+        Banco.conectar();
+        
+        //cria o comando preparado
+        pst = Banco.obterConexao().prepareStatement(sql);
+        
+        //coloca os valores dentro do comando
+        //substitui as '?' por dados
+        pst.setInt(1, model.getId());
+        pst.setString(2, model.getNome());
+        pst.setBoolean(3, model.getPrescricao());
+        pst.setString(4, model.getDescricao());
+        pst.setFloat(5, model.getPreco());
+        
+        //executa o comando
+        if(pst.executeUpdate() >= 1) { //tudo certo
+            Banco.desconectar();
+            return true;
+        }
+        else {
+            Banco.desconectar();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean remove(Produto model) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean altera(Produto model) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Produto buscarID(Produto model) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Collection<Produto> listar(String criterio) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
