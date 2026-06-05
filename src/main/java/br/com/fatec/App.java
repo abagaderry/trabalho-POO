9ce1befcb16f8f3b110c96da9144693aeb221627
Katchuga <biel2005pereira@gmail.com>
2026-06-05 14:38:53 -0300

    atulização na DAO e no controlador de cliente, update funciona

--- src/main/java/br/com/fatec/App.java
+++ src/main/java/br/com/fatec/App.java
@@ -1,8 +1,6 @@
 package br.com.fatec;
 
-import br.com.fatec.DAO.ClienteDAO;
 import br.com.fatec.DAO.ProdutoDAO;
-import br.com.fatec.model.Cliente;
 import br.com.fatec.model.Produto;
 import br.com.fatec.persistencia.Banco;
 import javafx.application.Application;
@@ -13,7 +11,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Collection;
 
 /**
  * JavaFX App
@@ -24,7 +21,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("tela_inicial"));
+        scene = new Scene(loadFXML("???????????????"));
         stage.setScene(scene);
         stage.show();
     }
@@ -34,11 +31,38 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/" + fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
-        launch();        
+        //launch();
+        try
+        {
+            //criar uma DAO
+            ProdutoDAO dao = new ProdutoDAO();
+            
+            /**buscar um proprietario
+            Produto p = dao.buscarID(new Produto(21, null));
+            
+            if(p == null) 
+                System.out.println("Proprietario não encontrado...");
+            else 
+            {
+                System.out.println("Código: " + p.getCodProprietario());
+                System.out.println("Nome: " + p.getNome());
+            }
+            */
+            //vamos inserir um dado
+            if(dao.insere(new Produto(1, "Dipirona", false, "toma ai", 2)))
+              System.out.println("Incluido com sucesso!!");
+            else
+              System.out.println("Erro na Inclusão");
+        }
+        catch (SQLException ex) 
+        {
+            System.out.println("Erro: " + ex.getMessage());
+        }
     }
+
 }
